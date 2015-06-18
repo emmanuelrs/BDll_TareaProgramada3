e59c53a8b15f8d193bf824047a515409a446b76e
lserrano467 <luis_ser_san@hotmail.com>
2015-06-18 15:53:40 -0600

    singleton

---
+++ proyect/Proyecto/Proyecto/src/java/controlador/listaProductos.java
@@ -0,0 +1,27 @@
+package controlador;
+
+
+import java.util.*;
+
+public class listaProductos {
+    private static List<productosVendidos> productos;
+    private static Boolean instancia = false;
+
+    public static List<productosVendidos> getlista(){
+        if(instancia == false){
+            instancia = true;
+            productos = new ArrayList<productosVendidos>();
+            return productos;
+        }
+        return productos;  
+    }
+    public void setProducto(String id, int cant){
+        productosVendidos product = new productosVendidos(id, cant);
+        if(instancia == false){
+            instancia = true;
+            productos = new ArrayList<productosVendidos>();
+            productos.add(product);
+        }
+        productos.add(product);
+    } 
+}
