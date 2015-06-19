4ccdb2bae46f1988d237545a095d42ea8a08fca7
lserrano467 <luis_ser_san@hotmail.com>
2015-06-19 13:17:30 -0600

    correccion a procedimiento :+1:

--- Procedimientos/ActualizarInventario.sql
+++ Procedimientos/ActualizarInventario.sql
@@ -1,10 +1,10 @@
-CREATE OR REPLACE PROCEDURE actualizarInventario(idProduct number,cantidad number)
+CREATE OR REPLACE PROCEDURE actualizarInventario(idProducto number,cantidad number)
 IS
   valorActual NUMBER(10);
   varlor NUMBER(10);
 BEGIN
   Select cantidad into valorActual from producto where id_producto = idProducto;
-  valor := valorActual - cantidad;
-  Update Producto set cantidad = valor where id_producto = idProducto;
+  valorActual := valorActual - cantidad;
+  Update Producto set cantidad = valorActual where id_producto = idProducto;
   COMMIT;
 END;
